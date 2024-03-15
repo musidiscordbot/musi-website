1c4e16e6cf11c2391d1d972eb52c4b13ac37afe3
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-15 14:51:25 +0000

    Update redirect.js

--- src/pages/redirect.js
+++ src/pages/redirect.js
@@ -33,23 +33,22 @@ export default function RedirectPage() {
       <Head>
         <title>Redirecting...</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://cdn.pylexservers.xyz/favicon.png" />
-        <link rel="stylesheet" href="/css/style.css" />
-        <link rel="stylesheet" href="/css/redirect.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <link rel="stylesheet" href="/css/bg.css" />
-        <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <Navbar />
+      <nav className="accordion arrows" id="commands">
+        <header className="box">
+          <label htmlFor="acc-close" className="box-title h-color">
+            Redirecting...
+          </label>
+        </header>
+      </nav>
       <div className="redirect-center">
         <a className="redirect-title">Redirecting to {router.query.url ? <span className="blue-clr">{router.query.url}</span> : null}!</a>
-        <img src="https://media.tenor.com/iUoxFQGhxcAAAAAj/pig-mine.gif" className="pig-running" />
         <a className="redirect-countdown">
           You'll be automatically redirected in <span className="actual-countdown">5 seconds</span>
           <span id="dots-loop">...</span>
         </a>
-
-        <button onClick={() => router.push(router.query.url)} className="redirect-faster">Continue</button>
 
         <script
           data-cfasync="false"
@@ -75,8 +74,6 @@ export default function RedirectPage() {
             `
           }}
         />
-
-        <a className="madewithlov">Made with <i className="fa-solid fa-heart"></i> by David D.</a>
       </div>
       <Footer />
     </motion.div>
