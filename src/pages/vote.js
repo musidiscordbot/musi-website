6135fdc59176bfe55a0fbbf9eaa9d9db4a24fd22
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-24 14:41:26 +0000

    Update vote.js

--- src/pages/vote.js
+++ src/pages/vote.js
@@ -29,11 +29,11 @@ export default function Commands() {
       <Head>
     <meta property="og:title" content="musi.lol/vote" />
         <meta property="og:description" content="The best free discord music bot." />
-        <meta property="og:image" content="https://i.ibb.co/h7nzKk6/musi-favicon-white-removebg-preview.png" />  {/* Replace with your image URL */}
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1216083221712736336/1221296058727010475/Musi_logo_7_trans_whiteText.png?ex=66120f96&is=65ff9a96&hm=69d0b5d2ae9a228caf8be8b0b50aaa0dabea7e9583ad5a8517021531a1f90f25&" />  {/* Replace with your image URL */}
         <meta property="og:url" content="https://musi.lol" />  {/* Replace with your website URL */}
         <title>Musi.lol - Voting</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="https://i.ibb.co/h7nzKk6/musi-favicon-white-removebg-preview.png" />
+        <link rel="icon" href="https://media.discordapp.net/attachments/1216083221712736336/1221296058727010475/Musi_logo_7_trans_whiteText.png?ex=66120f96&is=65ff9a96&hm=69d0b5d2ae9a228caf8be8b0b50aaa0dabea7e9583ad5a8517021531a1f90f25&" />
       </Head>
       <Navbar />
       <section className="about-section">
