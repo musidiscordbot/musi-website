5b3bae7d157f130d2853f9e3f63481eeb84d312c
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-26 22:06:31 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,9 @@
 import Link from "next/link";
 
+const openPopup = (url) => {
+  window.open(url, 'popupWindow', 'width=600,height=400');
+};
+
 const Navbar = () => {
   return (
     <div>
@@ -17,27 +21,23 @@ const Navbar = () => {
           <ul>
             <li>
               <Link className="p-color" href="/">
-            Home
-              </Link>
-              </li>
-              <li>
-              <Link className="p-color" href="https://discord.com/oauth2/authorize?client_id=1137417865016918149&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fmusi.lol%2Fadded&scope=bot+identify">
-          Add
-              </Link>
-                      </li>
-              <li>
-                 <Link className="p-color" href="/status">
-              Status
-              </Link>
-                  </li>
-              <li>
-                 <Link className="p-color" href="/premium">
-              Premium
+                Home
               </Link>
             </li>
             <li>
+              <a className="p-color" onClick={() => openPopup('https://discord.com/oauth2/authorize?client_id=1137417865016918149&permissions=8&response_type=code&redirect_uri=https%3A%2F%2Fmusi.lol%2Fadded&scope=bot+identify')}>
+                Add
+              </a>
             </li>
             <li>
+              <Link className="p-color" href="/status">
+                Status
+              </Link>
+            </li>
+            <li>
+              <Link className="p-color" href="/premium">
+                Premium
+              </Link>
             </li>
           </ul>
         </nav>
