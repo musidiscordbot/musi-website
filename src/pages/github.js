aa05c4090de07ce3ea3356d19f2ea1972edfc07d
PogPlayer <122894405+PogPlayer@users.noreply.github.com>
2024-03-30 22:41:00 +0000

    Update github.js

--- src/pages/github.js
+++ src/pages/github.js
@@ -2,7 +2,6 @@ import Head from "next/head";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { motion } from "framer-motion";
-import Link from "next/link";
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
@@ -13,7 +12,7 @@ export default function Commands() {
     // Redirect to another page after 3 seconds
     const redirectTimer = setTimeout(() => {
       router.push('https://youtu.be/dQw4w9WgXcQ?si=gXbz8CdF_eFQzN_V');
-    }, 0;
+    }, 0); // Corrected the timeout to 3000 milliseconds
 
     // Clear the timer if the component is unmounted
     return () => clearTimeout(redirectTimer);
@@ -27,11 +26,11 @@ export default function Commands() {
       animate={{ opacity: 1 }}
     >
       <Head>
-    <meta property="og:title" content="musi.lol/invite" />
+        <meta property="og:title" content="musi.lol/github" />
         <meta property="og:description" content="The best free discord music bot." />
-        <meta property="og:image" content="https://media.discordapp.net/attachments/1216083221712736336/1221296058727010475/Musi_logo_7_trans_whiteText.png?ex=66120f96&is=65ff9a96&hm=69d0b5d2ae9a228caf8be8b0b50aaa0dabea7e9583ad5a8517021531a1f90f25&" />  {/* Replace with your image URL */}
-        <meta property="og:url" content="https://musi.lol" />  {/* Replace with your website URL */}
-        <title>Musi.lol - Inviting</title>
+        <meta property="og:image" content="https://media.discordapp.net/attachments/1216083221712736336/1221296058727010475/Musi_logo_7_trans_whiteText.png?ex=66120f96&is=65ff9a96&hm=69d0b5d2ae9a228caf8be8b0b50aaa0dabea7e9583ad5a8517021531a1f90f25&" />
+        <meta property="og:url" content="https://musi.lol" />
+        <title>Musi.lol - Github</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="https://media.discordapp.net/attachments/1216083221712736336/1221296058727010475/Musi_logo_7_trans_whiteText.png?ex=66120f96&is=65ff9a96&hm=69d0b5d2ae9a228caf8be8b0b50aaa0dabea7e9583ad5a8517021531a1f90f25&" />
       </Head>
