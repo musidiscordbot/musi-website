a2618e4ea9347c40ccef621fa11e2a08318015f7
thatdevwolfy <122894405+thatdevwolfy@users.noreply.github.com>
2024-04-21 02:32:33 +0100

    Update premium.js

--- src/pages/premium.js
+++ src/pages/premium.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
+import Link from "next/link";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { motion } from "framer-motion";
-import Link from "next/link";
 
-export default function Premium() {
+export default function Privacy() {
   return (
     <motion.div
       initial={{
@@ -13,34 +13,59 @@ export default function Premium() {
       animate={{ opacity: 1 }}
     >
       <Head>
-        <title>Premium | Musi</title>
+        <title>Musi.lol - Premium</title>
+        <meta property="og:title" content="musi.lol/privacy" />
+        <meta property="og:description" content="The best free Discord music bot." />
+        <meta property="og:image" content="https://example.com/your-image.jpg" />
+        <meta property="og:url" content="https://musi.lol" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="https://example.com/your-favicon.ico" />
       </Head>
       <Navbar />
       <section className="about-section">
-        <nav className="accordion arrows" id="commands">
+        <nav className="accordion arrows" id="privacy-policy">
           <header className="box">
             <label htmlFor="acc-close" className="box-title h-color">
-              Soon....
+              Musi Premium
             </label>
           </header>
-      <button className="btn">
-          <Link className="h-color no-decoration" href="https://musi.lol/invite">
-            Invite Musi
-          </Link>
-        </button>
-          <button className="btn">
-          <Link className="h-color no-decoration" href="https://discord.gg/YNTTT2EPqC">
-            Join the support server
-          </Link>
-        </button>
-        <button className="btn">
-          <Link className="h-color no-decoration" href="https://top.gg/bot/1137417865016918149/vote">
-            Vote for musi!
-          </Link>
-        </button>
           
+          <input type="radio" name="accordion" id="cb1" />
+          <section className="box">
+            <label className="box-title p-color" htmlFor="cb1">
+              Why Choose Premium?
+            </label>
+            <label className="box-close" htmlFor="acc-close"></label>
+            <div className="box-content p-color">
+              <ul>
+                <li>1. Ad-Free Experience</li>
+                <li>2. Exclusive Features</li>
+                <li>3. High-Quality Audio</li>
+                <li>4. Priority Support</li>
+                <li>5. Customization Options</li>
+                <li>6. Early Access to Updates</li>
+                <li>7. Enhanced Stability and Performance</li>
+                <li>8. Community Recognition</li>
+                <li>9. Exclusive Events and Giveaways</li>
+                <li>10. Supporting Development</li>
+              </ul>
+            </div>
+          </section>
+          <button className="btn">
+            <Link href="https://musi.lol/invite">
+              <a className="h-color no-decoration">Invite Musi</a>
+            </Link>
+          </button>
+          <button className="btn">
+            <Link href="https://discord.gg/YNTTT2EPqC">
+              <a className="h-color no-decoration">Join the support server</a>
+            </Link>
+          </button>
+          <button className="btn">
+            <Link href="https://top.gg/bot/1137417865016918149/vote">
+              <a className="h-color no-decoration">Vote for Musi!</a>
+            </Link>
+          </button>
         </nav>
       </section>
       <Footer />
